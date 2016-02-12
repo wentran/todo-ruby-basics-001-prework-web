bf0584dda29e24db1847f24d42840c67c130f62e
Wen Tran <wentran@Wen-Trans-MacBook-Pro.local>
2016-02-11 23:21:10 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,38 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
+
 
 def assign_variable(value)
-  
+    return value
 end
 
-def argue
-  
+
+def argue (phrase)
+phrase
 end
 
-def greeting
 
+
+def greeting (greeting, name)
+greeting 
+name
 end
+
 
 def return_a_value
-
+return "Nice"
 end
 
 def last_evaluated_value
-
+return "expert"
 end
 
-def pizza_party
-
+def pizza_party (value= "cheese")
+value
 end
+
+
+
+
+
